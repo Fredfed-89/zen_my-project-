750532f891728c35b37d3902752417ddf0587ece
Fredfed-89 <fedyaev.sergei89@gmail.com>
2022-08-02 00:21:17 +0300

    end of animations

--- src/js/components/sliders.js
+++ src/js/components/sliders.js
@@ -1,13 +1,13 @@
-import Swiper from 'swiper/bundle';
+// import Swiper from 'swiper/bundle';
 
-const mySwiper = new Swiper('.swiper', {
-  // modules: [Navigation, Pagination],
-  slidesPerView: 1,
-  spaceBetween: 30,
-  loop: true,
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-});
+// const mySwiper = new Swiper('.swiper', {
+//   // modules: [Navigation, Pagination],
+//   slidesPerView: 1,
+//   spaceBetween: 30,
+//   loop: true,
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+// });
--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,8 @@
-import './_vendor';
-import vars from './_vars';
+// import './_vendor';
+// import vars from './_vars';
 import './_functions';
-import './_components';
-import './components/sliders';
+// import './_components';
+// import './components/sliders';
 import './components/animation';
 import './components/smooth';
 import './components/hamburger';
